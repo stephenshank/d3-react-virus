03e2e6e45371f7474e7614e703231545407af2a4
stephenshank <sshank314@gmail.com>
2020-01-25 12:47:17 -0500

    Basic envelope component.

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,62 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Envelope from "./envelope.jsx";
 
 import "./styles.scss";
 
 function App() {
+  const width = 200,
+    height = 200;
   return (<div>
     <Navbar bg="light" expand="lg">
       <Navbar.Brand href="#home">D3-React Virus</Navbar.Brand>
     </Navbar>
+    <Container>
+      <Row>
+        <Col>
+          <h1>Envelope</h1>
+          <svg
+            width={width}
+            height={height}
+            style={{
+              borderStyle: "solid",
+              borderWidth: 1,
+              borderColor: "grey"
+            }}
+          >
+            <rect
+              x={width/4}
+              y={height/4}
+              width={width/2}
+              height={height/2}
+              fill={"#EEEEEE"}
+            />
+            <line
+              x1={width/2}
+              x2={width/2}
+              y1={0}
+              y2={height}
+              stroke="lightgrey"
+            />
+            <line
+              x1={0}
+              x2={width}
+              y1={height/2}
+              y2={height/2}
+              stroke="lightgrey"
+            />
+            <Envelope
+              cx={width/2}
+              cy={width/2}
+            />
+          </svg>
+        </Col>
+      </Row>
+    </Container>
   </div>);
 }
 
---
+++ src/envelope.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { path } from "d3";
+
+function Envelope(props) {
+  const { width, height } = props,
+    translateX = props.cx - width/2,
+    translateY = props.cy - props.height/2,
+    etop = path(),
+    estem = path(),
+    esteml = path(),
+    estemr = path();
+
+  etop.moveTo(width/4, height/6);
+  etop.quadraticCurveTo(width/4, 0, width/2, 0);
+  etop.quadraticCurveTo(3*width/4, 0, 3*width/4, height/6);
+  etop.quadraticCurveTo(3*width/4, height/3, width/2, height/3);
+  etop.quadraticCurveTo(width/4, height/3, width/4, height/6);
+
+  estem.moveTo(width/3, height/6);
+  estem.quadraticCurveTo(width/2, height/2, width/4, height);
+  estem.quadraticCurveTo(width/2, height, 3*width/4, height);
+  estem.quadraticCurveTo(width/2, height/2, 2*width/3, height/6);
+  estem.moveTo(width/3, height/6);
+
+  esteml.moveTo(width/3, height/6);
+  esteml.quadraticCurveTo(width/2, height/2, width/4, height);
+
+  estemr.moveTo(2*width/3, height/6);
+  estemr.quadraticCurveTo(width/2, height/2, 3*width/4, height);
+  return (<g
+      transform={`translate(${translateX},${translateY})`}
+    >
+    <path
+      d={estem.toString()}
+      fill="red"
+    />
+    <path
+      d={esteml.toString()}
+      stroke="black"
+      strokeWidth={2}
+      fill="none"
+    />
+    <path
+      d={estemr.toString()}
+      stroke="black"
+      strokeWidth={2}
+      fill="none"
+    />
+    <path
+      d={etop.toString()}
+      stroke="black"
+      strokeWidth={2}
+      fill="red"
+    />
+  </g>)
+}
+
+Envelope.defaultProps = {
+  width: 100,
+  height: 100 
+};
+
+export default Envelope;
