1c84034f6ab30f8b1cfbc60b49e55340a63878ab
stephenshank <sshank314@gmail.com>
2020-01-28 08:04:40 -0500

    Envelope top shading.

--- src/envelope.jsx
+++ src/envelope.jsx
@@ -10,13 +10,25 @@ function Envelope(props) {
     etop = path(),
     estem = path(),
     esteml = path(),
-    estemr = path();
+    estemr = path(),
+    etop_shadow = path(),
+    etop_left = width/4,
+    etop_middle = width/2,
+    etop_right = 3*width/4,
+    etop_top = 0,
+    etop_center = height/6,
+    etop_bottom = height/3;
 
-  etop.moveTo(width/4, height/6);
-  etop.quadraticCurveTo(width/4, 0, width/2, 0);
-  etop.quadraticCurveTo(3*width/4, 0, 3*width/4, height/6);
-  etop.quadraticCurveTo(3*width/4, height/3, width/2, height/3);
-  etop.quadraticCurveTo(width/4, height/3, width/4, height/6);
+  etop.moveTo(etop_left, etop_center);
+  etop.quadraticCurveTo(width/4, 0, etop_middle, etop_top);
+  etop.quadraticCurveTo(3*width/4, 0, etop_right, etop_center);
+  etop.quadraticCurveTo(3*width/4, height/3, etop_middle, etop_bottom);
+  etop.quadraticCurveTo(width/4, height/3, etop_left, etop_center);
+
+  etop_shadow.moveTo(etop_right, etop_center);
+  etop_shadow.quadraticCurveTo(3*width/4, height/3, etop_middle, etop_bottom);
+  etop_shadow.quadraticCurveTo(width/4, height/3, etop_left, etop_center);
+  etop_shadow.quadraticCurveTo(width/2, 2*height/5, etop_right, etop_center);
 
   estem.moveTo(width/3, height/6);
   estem.quadraticCurveTo(width/2, height/2, width/4, height);
@@ -33,28 +45,36 @@ function Envelope(props) {
       transform={translateString}
     >
     <g transform={rotateString}>
-    <path
-      d={estem.toString()}
-      fill="red"
-    />
-    <path
-      d={esteml.toString()}
-      stroke="black"
-      strokeWidth={2}
-      fill="none"
-    />
-    <path
-      d={estemr.toString()}
-      stroke="black"
-      strokeWidth={2}
-      fill="none"
-    />
-    <path
-      d={etop.toString()}
-      stroke="black"
-      strokeWidth={2}
-      fill="red"
-    />
+      <path
+        d={estem.toString()}
+        fill="red"
+      />
+      <path
+        d={esteml.toString()}
+        stroke="black"
+        strokeWidth={2}
+        fill="none"
+      />
+      <path
+        d={estemr.toString()}
+        stroke="black"
+        strokeWidth={2}
+        fill="none"
+      />
+      <path
+        d={etop.toString()}
+        fill="red"
+      />
+      <path
+        d={etop_shadow.toString()}
+        fill="DarkRed"
+      />
+      <path
+        d={etop.toString()}
+        stroke="black"
+        strokeWidth={2}
+        fill="none"
+      />
     </g>
   </g>)
 }
