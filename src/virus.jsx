6a8fd95e10e8a4a80962eeb6f67e7e47c4ff82ec
stephenshank <sshank314@gmail.com>
2020-01-28 08:04:55 -0500

    Virus membrane shading.

--- src/virus.jsx
+++ src/virus.jsx
@@ -1,21 +1,48 @@
 import React from "react";
-import { range } from "d3";
+import { range, path, scaleLinear } from "d3";
 import Envelope from "./envelope.jsx";
 
 function Virus(props) {
   const { width, height, outerEnvelopes } = props,
     cx = width/2,
     cy = height/2,
+    r_membrane = width/6,
+    r_membrane_padded = r_membrane - 2,
     r_env = width/5,
-    translateString = `translate(${props.translateX},${props.translateY})`;
+    translateString = `translate(${props.translateX},${props.translateY})`,
+    shadow = path(),
+    shadow_scale = scaleLinear();
+
+  shadow_scale.domain([0, 100])
+    .range([-3*Math.PI/4, Math.PI/4]);
+  shadow.moveTo(
+    cx + r_membrane_padded*Math.cos(shadow_scale(0)),
+    cy - r_membrane_padded*Math.sin(shadow_scale(0)),
+  );
+  for(let i=1; i < 100; i++) {
+    shadow.lineTo(
+      cx + r_membrane_padded*Math.cos(shadow_scale(i)),
+      cy - r_membrane_padded*Math.sin(shadow_scale(i)),
+    );
+  }
+  shadow.quadraticCurveTo(
+    cx + 1.1*r_membrane,
+    cy + 1.1*r_membrane,
+    cx + r_membrane_padded*Math.cos(shadow_scale(0)),
+    cy - r_membrane_padded*Math.sin(shadow_scale(0)),
+  );
   return (<g transform={translateString}>
     <circle
       cx={cx}
       cy={cy}
-      r={width/6}
+      r={r_membrane}
       fill="red"
       stroke="black"
       strokeWidth={4}
+    />
+    <path
+      d={shadow.toString()}
+      fill="DarkRed"
     />
     {range(outerEnvelopes).map(i => {
       const angle_radians = 2*Math.PI*i/outerEnvelopes,
@@ -27,8 +54,8 @@ function Virus(props) {
         key={i}
         cx={cx_i}
         cy={cy_i}
-        width={50}
-        height={50}
+        width={props.envelopeSize}
+        height={props.envelopeSize}
         rotate={angle_degrees}
       />);
     })}
@@ -41,7 +68,8 @@ Virus.defaultProps = {
   translateY: 0,
   width: 400,
   height: 400,
-  outerEnvelopes: 10
+  outerEnvelopes: 10,
+  envelopeSize: 50
 };
 
 export default Virus;
